14831a18ee74b02a54bf6ae6fae7a5d62c45d65a
rayondemiel <maximehumeau44@gmail.com>
2022-07-28 14:40:15 -0400

    generate id by ent and restructuration

--- src/opt/utils.py
+++ src/opt/utils.py
@@ -1,24 +1,27 @@
 import re
 import datetime
 import json
+import numpy as np
 
 
-def date_process():
-    x = datetime.datetime.now()
-    return x.strftime("%Y-%m-%d")
+class Tools:
+    @staticmethod
+    def date_process():
+        x = datetime.datetime.now()
+        return x.strftime("%Y-%m-%d")
 
+    @staticmethod
+    def replace_pattern(input_txt: str, pattern: str, replace: str) -> str:
+        r = re.findall(pattern, input_txt)
+        for i in r:
+            input_txt = re.sub(i, replace, input_txt)
+        return input_txt
 
-def replace_pattern(input_txt: str, pattern: str, replace: str) -> str:
-    r = re.findall(pattern, input_txt)
-    for i in r:
-        input_txt = re.sub(i, replace, input_txt)
-    return input_txt
-
-
-def read_json(file: str, encoding="utf-8"):
-    with open(file, encoding=encoding, mode="r") as f:
-        f_json = json.load(f)
-    return f_json
+    @staticmethod
+    def read_json(file: str, encoding="utf-8"):
+        with open(file, encoding=encoding, mode="r") as f:
+            f_json = json.load(f)
+        return f_json
 
 
 ###build a fumction to find if id exist. if exist find id in csv and put in ref
@@ -26,11 +29,14 @@ def read_json(file: str, encoding="utf-8"):
 
 class DataTools:
 
-    def generate_id(self):
+    @staticmethod
+    def generate_id(ent_: str) -> str:
         """
         find id if existing in JSON files, if not to create it
         :return:
         """
+
+        return ent_ + "_" + str(np.random.random_integers(10000, high=99999))
 
     def verification(self):
         """
