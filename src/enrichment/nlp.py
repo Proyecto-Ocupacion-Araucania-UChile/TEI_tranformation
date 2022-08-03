b096d4889e572831bffa6bc0ddd68840af128295
rayondemiel <maximehumeau44@gmail.com>
2022-08-03 18:57:10 +0200

    modif gitignore

--- src/enrichment/nlp.py
+++ src/enrichment/nlp.py
@@ -1,20 +1,32 @@
 import spacy
+from lxml import etree as ET
+from collections import namedtuple
 
 
 class NLP:
     Text = None
+    GPU = False
 
-    def __init__(self, model):
+    def __init__(self, root, model):
         self.model = model
         self.nlp = self._init_model_()
+        self.root = root
 
     def _init_model_(self):
-        spacy.prefer_gpu()
+        self.GPU = spacy.prefer_gpu()
         return spacy.load(self.model)
 
-    def
+    def __gpu__(self):
+        print('GPU:', self.GPU)
+
+    @staticmethod
+    def text_compilation() -> list:
+        # find all subelement in root (body)
+        # return list list of nametuple with element and text
+        return list
 
     def preprocessing(self):
         return
 
     def processing(self):
+        return
